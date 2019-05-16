586f4d4adf23719b782f28ad313246f5982f3b89
antonio.martin <antonio.martin@schibsted.com>
2019-05-16 09:08:21 +0200

    adding bd support!

--- src/main/kotlin/com/amm/scheduler/SchedulerApplication.kt
+++ src/main/kotlin/com/amm/scheduler/SchedulerApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@ComponentScan
 @EnableScheduling
 class SchedulerApplication
 
